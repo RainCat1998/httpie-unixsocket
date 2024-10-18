6d0bd945db53ac73403e7f46b9c2558fa6681a7d
Yao Wentao <36730607+RainCat1998@users.noreply.github.com>
2024-10-19 03:05:37 +0800

    Update httpie_unixsocket.py

--- httpie_unixsocket.py
+++ httpie_unixsocket.py
@@ -3,13 +3,20 @@ UNIX socket transport plugin for HTTPie.
 
 """
 from httpie.plugins import TransportPlugin
-from requests_unixsocket import UnixAdapter, DEFAULT_SCHEME
+from requests.compat import urlparse
+from requests_unixsocket import UnixAdapter, Settings, DEFAULT_SCHEME
 
 
 __version__ = '0.0.0'
 __author__ = 'Marc Abramowitz'
 __licence__ = 'BSD'
 
+def custom_urlparse(url):
+    parsed_url = urlparse(url)
+    return Settings.ParseResult(
+        sockpath=parsed_url.path,
+        reqpath=parsed_url.fragment,
+    )
 
 class UnixSocketTransportPlugin(TransportPlugin):
     name = 'UNIX socket transport'
@@ -17,4 +24,4 @@ class UnixSocketTransportPlugin(TransportPlugin):
     prefix = DEFAULT_SCHEME
 
     def get_adapter(self):
-        return UnixAdapter()
+        return UnixAdapter(settings=Settings(urlparse=custom_urlparse))
